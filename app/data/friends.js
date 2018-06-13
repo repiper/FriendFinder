3dc98bf15b105e738c62864013a0a950db3f75cb
Risa Piper <risapiper@208-73-101-105.fttp.usinternet.com>
2018-06-13 16:14:09 -0500

    Initial Commit

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+	{
+		name: 'Priyanka Chopra',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMjAxNzUwNjExOV5BMl5BanBnXkFtZTcwNDUyMTUxNw@@._V1_.jpg',
+		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+	},{
+		name: 'John Oliver',
+		photo: 'https://www.hbo.com/content/dam/hbodata/series/last-week-tonight-with-john-oliver/episodes/s04/170925-lwtwjo-ep113-1920.jpg/_jcr_content/renditions/cq5dam.web.1200.675.jpeg',
+		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+	},{
+		name: 'Amy Schumer',
+		photo: 'https://shachanmedia.files.wordpress.com/2015/06/amy-schumer.jpg',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: 'Trevor Noah',
+		photo: 'http://d2752rta4dscqv.cloudfront.net/ImageCache/trevor_noah_h_0516_1.d0172cf47fb9262005552e22ca1e8f9d.jpg',
+        scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+    },{   
+		name: 'Ricky Martin',
+		photo: 'http://thinkmagazines.com/wp-content/uploads/2017/10/Ricky-Martin-1280.jpg',
+		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+	},{
+		name: 'Drake',
+		photo: 'https://www.billboard.com/files/media/drake-nba-awards-smiling-2017-billboard-1548.jpg',
+		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+	}];
+
+module.exports = friends;
+
+http://thinkmagazines.com/wp-content/uploads/2017/10/Ricky-Martin-1280.jpg
